1ef1aa7a56b9756a03e3a2acded1bc8d8f701a8e
bogdansergeevicburduluk <bogdanburduluk@gmail.com>
2023-04-19 19:59:23 +0200

    Added info about day-12

---
+++ Other code/other_day_12.py
@@ -0,0 +1,54 @@
+### Scope ###
+
+# enemies = 1
+#
+# def increase_enemies():
+#     print(f"enemies inside function: {enemies}")
+#     return enemies + 1
+#
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+
+# Local Scope
+# def drink_potion():
+#     potion_strength = 2
+#     print(potion_strength)
+#
+# drink_potion()
+
+ # Global Scope
+
+# plater_health = 10
+#
+# def game():
+#     def drink_potion():
+#         potion_strength = 2
+#         print(plater_health)
+#
+#         drink_potion()
+#
+# print(plater_health)
+
+# game_level = 3
+# def create_enemy():
+#     enemies = ["Skeleton", "Zombie", "Alies"]
+#     if game_level < 5:
+#         new_enenmy = enemies[0]
+#
+#     print(new_enemy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
