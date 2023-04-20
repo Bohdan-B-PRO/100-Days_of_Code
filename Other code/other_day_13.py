eacdffb488877a47abdcc4edc4a4de0abc79580a
bogdansergeevicburduluk <bogdanburduluk@gmail.com>
2023-04-20 12:41:00 +0200

    Added info about day-13

---
+++ Other code/other_day_13.py
@@ -0,0 +1,71 @@
+# Debugging
+
+# Describe Problem
+# def my_function():
+#     for i in range(1, 21):
+#         if i == 20:
+#             print("You get it")
+# my_function()
+
+
+# Reproduce the Bug
+# from random import randint
+# dice_imps = ["0", '0', "0", "0", "0", "0"]
+# dice_num = randint(0, 5)
+# print(dice_imps[dice_num])
+
+
+# Play Computer
+# year = int(input("What's your year of birth? "))
+# if year > 2000 and year <= 1994:
+#     print("You're a millenial")
+# elif year > 1994:
+#     print("You're Gen 2.")
+
+
+# Fix the Errors
+# age = int(input("How old are you? "))
+# if age > 18:
+#     print(f"You can drive at age {age}")
+
+
+# Print 9s Your Friend
+# pages = 0
+# word_oer_page = 0
+# pages = int(input("number of page: "))
+# word_oer_page = int(input("Number of words oer page: "))
+# total_words = pages * word_oer_page
+# print(f"pages = {pages}")
+# print(f"word_per_page = {word_oer_page}")
+# print(total_words)
+
+
+# Use a Debugger
+# def mutate(a_list):
+#     b_list = []
+#     for item in a_list:
+#         new_item = item * 2
+#         b_list.append(new_item)
+#         print(b_list)
+#
+# mutate([1, 2, 3, 4, 5, 8, 13])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
