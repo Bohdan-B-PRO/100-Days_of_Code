f590cb04cdc55556fe3856735bce9e356b44c2a6
bogdansergeevicburduluk <bogdanburduluk@gmail.com>
2023-05-11 16:03:19 +0200

    Added information of day 26

---
+++ Other code/other_day_26.py
@@ -0,0 +1,52 @@
+# PART 1
+# numbers = [1, 2, 3]
+# new_list = [n + 1 for n in numbers]
+# print(new_list)
+#
+# name = "Bohdan"
+# letters_list = [letter for letter in name]
+# print(letters_list)
+#
+# range_list = [num * 2 for num in range(1, 5)]
+# print(range_list)
+#
+# names = ["Alex", "Beth", "Caroline", "Dave", "Freddie"]
+# short_names = [name for name in names if len(name) < 5]
+# long_names = [name.upper() for name in names if len(name) > 5]
+# print(short_names)
+# print(long_names)
+
+
+# PART 2
+# numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+# squared_numbers = [num**2 for num in numbers]
+# print(squared_numbers)
+
+# result = [num for num in numbers if num % 2 == 0]
+# print(result)
+
+
+# PART 3
+# import random
+# names = ["Alex", "Beth", "Caroline", "Dave", "Freddie"]
+# students_scores = {student:random.randint(1, 100) for student in names}
+# passed_students = {student:score for (student, score) in students_scores.items() if score >= 60 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
