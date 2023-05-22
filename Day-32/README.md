6089c8e8eba3601f951c86abdd824c011a6e4606
bogdansergeevicburduluk <bogdanburduluk@gmail.com>
2023-05-22 10:51:28 +0200

    Added information of day 32

---
+++ Day-32/README.md
@@ -0,0 +1,9 @@
+# Day 32 - Send Email (smtplib) & Manage Dates (datetime)
+---
+## Concepts Practised
+___
+* How to Send Emails with Python using SMTP
+* Working with the datetime Module
+___
+## Automated Birthday Wisher
+![](https://user-images.githubusercontent.com/98851253/155895378-f5e03b78-e2f9-4d3c-846e-f5ecdb43a259.gif)
---
+++ Other code/other_day_32.py
@@ -0,0 +1,35 @@
+# import smtplib
+#
+# my_email = "bogdanburduluk@gmail.com"
+# password = "12345_B_B_B"
+#
+# with smtplib.SMTP("gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="bogdanburduluk@gmail.com",
+#         msg="Subjeck:Hello\n\nThi's the body of my email"
+#     )
+
+
+# import datetime as dt
+#
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of_week = now.weekday()
+# print(day_of_week)
+#
+# data_of_birth = dt.datetime(year=1998, month=3, day=14)
+
+
+
+
+
+
+
+
+
+
+
